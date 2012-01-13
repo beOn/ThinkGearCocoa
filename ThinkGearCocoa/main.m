8247efa610d4952607290a131da317d2e7c6c47c
Ben Acland <ben.acland@hulu.com>
2012-01-12 20:28:39 -0800

    Initial Commit

---
+++ ThinkGearCocoa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ThinkGearCocoa
+//
+//  Created by Ben Acland on 1/12/12.
+//  Copyright (c) 2012 Hulu, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
